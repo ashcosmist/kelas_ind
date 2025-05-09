81532970d8374d165ef8948721bcafdd66315fd4
ashcosmist <ashcosmist@gmail.com>
2025-05-09 21:56:05 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,14 @@
+let books = [
+  {
+    "id": 1746595341475,
+    "title": "tst",
+    "category": "Fiksi",
+    "author": "test"
+  },
+  {
+    "id": 1746762181436,
+    "title": "tes",
+    "category": "Edukasi",
+    "author": "gweh"
+  }
+];module.exports = { books };
